c7c4d5a2a5132e962c06853bc2e1eee2ddff795f
ElizabethSharonZimba <sharonzimba95@gmail.com>
2024-03-17 03:34:24 -0700

    14 and ity

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,32 +1,31 @@
 #!/usr/bin/python3
-
-"""
-This script prints all City objects
-from the database `hbtn_0e_14_usa`.
-"""
-
-from sys import argv
-from model_state import State, Base
-from model_city import City
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.schema import Table
- 
+from model_state import Base, State
+from model_city import City
+
+def fetch_cities_by_state(username, password, database):
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(username, password, database),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    cities = session.query(City).order_by(City.id).all()
+
+    for city in cities:
+        state_name = session.query(State).filter_by(id=city.state_id).first().name
+        print("{}: ({}) {}".format(state_name, city.id, city.name))
+
+    session.close()
 
 if __name__ == "__main__":
-    """
-    Access to the database and get the cities
-    from the database.
-    """
-     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2],
-                                   sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
 
-    session = Session(engine)
-    for state, city in session.query(State, City)\
-                              .filter(City.state_id == State.id)\
-                              .order_by(City.id).all():
-            print("{}: ({}) {}".format(state.name, city.id, city.name))
-    session.close()
+    username, password, database = sys.argv[1:]
+
+    fetch_cities_by_state(username, password, database)
 
