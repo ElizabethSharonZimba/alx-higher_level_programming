e08930e64db5799698b102fc5c1b539a472d217b
ElizabethSharonZimba <sharonzimba95@gmail.com>
2024-03-17 02:45:37 -0700

    fixed 2

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,19 +9,34 @@ import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-    """
-    Access to the database and get the states
-    from the database.
-    """
-    db_connect = db.connect(host="localhost", port=3306,
-                            user=argv[1], passwd=argv[2], db=argv[3])
-    db_cursor = db_connect.cursor()
+    if len(argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(argv[0]))
+        exit(1)
 
-    db_cursor.execute(
-        "SELECT * FROM states WHERE name LIKE BINARY 'N%' \
-                ORDER BY states.id ASC")
+    username, password, database = argv[1:]
 
-    rows_selected = db_cursor.fetchall()
+    try:
+        db_connect = MySQLdb.connect(
+            host="localhost", port=3306, user=username,
+            passwd=password, db=database
+        )
+        db_cursor = db_connect.cursor()
 
-    for row in rows_selected:
-        print(row)
+        db_cursor.execute(
+            "SELECT * FROM states WHERE name LIKE BINARY 'N%' \
+                ORDER BY states.id ASC"
+        )
+
+        rows_selected = db_cursor.fetchall()
+
+        for row in rows_selected:
+            print(row)
+
+    except MySQLdb.Error as e:
+        print("MySQL Error: {}".format(str(e)))
+
+    finally:
+        if db_cursor:
+            db_cursor.close()
+        if db_connect:
+            db_connect.close()
